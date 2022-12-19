fe131a7801c889bd0643347a385889e1ce710fdb
sitampan21 <118861386+sitampan21@users.noreply.github.com>
2022-12-20 03:52:43 +0700

    Update user-routes.js

--- router/user-routes.js
+++ router/user-routes.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const database = require('../config/database');
 const router = express.Router();
-const bcrypt = require('bcrypt');
+const bcrypt = require('bcryptjs');
 
 
 router.post('/register', async (req, res) => {
